54afc65f89071e31595605fe143a1923548832fa
Pratyush Ranjan Srivastava <38191977+Pratyush-Ranjan@users.noreply.github.com>
2018-07-01 19:50:57 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,36 +1,36 @@
-var mongoose= require('mongoose');
-var bcrypt = require('bcryptjs')
-mongoose.connect('mongodb://localhost/globalbaba');
-var Schema= mongoose.Schema;
-var schema= new Schema ({
-	uname: {type: String, required: true},
-	email: {type: String, required: true},
-	password: {type: String, required: true }
-});
-module.exports=mongoose.model('users',schema);
-module.exports.comparePassword = function(candidatePassword ,hash,callback){
-	console.log('raghuvaran here');
-    bcrypt.compare(candidatePassword,hash,function(err,isMatch){
-       if(err) return callback(err);
-        callback(null,isMatch);
-    });
-}
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-module.exports.getUserByusername= function(username,callback){
-	console.log('gethere');
-    var query ={email :username};
-    User.findOne(query,callback);
-}
-
-module.exports.getUserById= function(id,callback){
-    User.findById(id,callback);
-}
-module.exports.createuser = function(newUser,callback){
-	bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(newUser.password, salt, function(err, hash) {
-        newUser.password = hash;
-        newUser.save(callback);
-    });
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  salt: String,
+  hash: String
 });
-   
-}
+
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64,`sha512`).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64,`sha512`).toString('hex');
+
+  return this.hash === hash;
+};
+
+module.exports=mongoose.model('users', UserSchema);
